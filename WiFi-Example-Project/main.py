e67cdf7d63d441946b92b3bd4bfd4469701550b2
Zichene <zichengao0318@gmail.com>
2023-11-06 16:54:14 -0500

    adding files for wifi example project see readme

---
+++ WiFi-Example-Project/main.py
@@ -0,0 +1,13 @@
+# This is a sample Python script.
+from convert_html_to_c import convert_html_to_c, clear_output_file
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+if __name__ == '__main__':
+    reset_output = True
+    if reset_output:
+        clear_output_file()
+    convert_html_to_c("http")
+
